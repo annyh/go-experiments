5d9778676b030a66df1aac039d15838a13060caa
Anny He <annyh@sfu.ca>
2021-04-17 21:00:26 -0700

    you can win game now

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
+// TODO: randomize word
 func main() {
 	count := 0
-	word := "hey"
+	word := "hello"
 	m := map[string]bool{}
+	w := getWordMap(word)
+	remaining := len(word)
+	fmt.Println("word has " + strconv.Itoa(remaining) + " characters")
+
 	for count < 6 {
 		input := getInput("")
 		_, ok := m[input]
@@ -15,9 +23,28 @@ func main() {
 		}
 		// string is unique
 		m[input] = true
+		// if string is one of the characters, say got character right
+		_, ok = w[input]
+		if ok {
+			fmt.Println(input + " is in the word.")
+			remaining -= int(w[input])
+			fmt.Println(strconv.Itoa(remaining) + " characters remaining")
+			if remaining == 0 {
+				fmt.Println("You won.")
+				break
+			}
+		}
 		count++
 	}
 	fmt.Println("It was " + word)
+}
+
+func getWordMap(word string) map[string]int32 {
+	w := map[string]int32{}
+	for _, c := range word {
+		w[string(c)]++
+	}
+	return w
 }
 
 func getInput(message string) string {
